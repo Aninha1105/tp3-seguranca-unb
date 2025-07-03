f11c38682910762ecff4bd176def4fccb6a3d0df
ogjoaoc <joaocarlgoncalvesfilho@gmail.com>
2025-07-03 12:24:57 -0300

    add number_theory

---
+++ number_theory.py
@@ -0,0 +1,69 @@
+import secrets
+
+primes = []
+
+def gcd(a, b):
+    if not b:
+        return a, 1, 0
+
+    d, x_, y_ = gcd(b, a % b)
+    x = y_
+    y = x_ - (a // b) * y_
+
+    return d, x, y
+
+def bin_pow(base, expo, MOD):
+    res = 1
+    base %= MOD
+    
+    while expo:
+        if expo & 1:
+            res = (res * base) % MOD
+        base = (base * base) % MOD
+        expo >>= 1
+        
+    return res
+
+def sieve(lim):
+    not_prime = set()
+    
+    for i in range(2, lim):
+        if i not in not_prime:
+            primes.append(i)
+            for j in range(2 * i, lim, i):
+                not_prime.add(j)
+    
+def composite(n, a, d, s):
+    x = bin_pow(a, d, n)
+
+    if x == 1 or x == n - 1:
+        return False
+
+    for _ in range(s):
+        x = (x * x) % n
+        if x == n - 1:
+            return False
+
+    return True
+
+def is_prime(n, k):
+    if n < 4:
+        return n == 2 or n == 3
+    
+    for p in primes:
+        if n % p == 0:
+            return False
+    
+    d = n - 1
+    s = 0
+
+    while (d & 1) == 0:
+        d >>= 1
+        s += 1
+    
+    for _ in range(k):
+        a = secrets.randbelow(n - 3) + 2
+        if composite(n, a, d, s):
+            return False
+    
+    return True
