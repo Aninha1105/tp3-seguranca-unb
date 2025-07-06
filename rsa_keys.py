93374d995e02992e002750703741a4149c95abea
ogjoaoc <joaocarlgoncalvesfilho@gmail.com>
2025-07-06 16:04:50 -0300

    add rsa key gen

--- rsa_keys.py
+++ rsa_keys.py
@@ -2,19 +2,13 @@ import number_theory
 import utils
 
 def generate_rsa_key_pair(bits):
-    p = number_theory.gen_prime(bits)
-    q = number_theory.gen_prime(bits)
-
-    n = p * q
-    phi = (p - 1) * (q - 1)
+    P = number_theory.gen_prime(bits)
+    Q = number_theory.gen_prime(bits)
+    N = P * Q
+    phi = (P - 1) * (Q - 1)
     e = 65537
+    D = number_theory.inverse_mod(e, phi)
+    utils.save_public_key("public_key.pem", N, e)
+    utils.save_private_key("private_key.pem", N, D)
 
-    # print(number_theory.gcd(e, phi))
-    # print(f"phi = {phi}")
-    d = number_theory.inverse_mod(e, phi)
-    
-
-    utils.save_public_key("public_key.pem", n, e)
-    utils.save_private_key("private_key.pem", n, d)
-
-generate_rsa_key_pair(1024)
+# generate_rsa_key_pair(1024)
