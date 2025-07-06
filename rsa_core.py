7f895e30e8845582efb0ee9195ad3ed776f2437b
ogjoaoc <joaocarlgoncalvesfilho@gmail.com>
2025-07-06 16:01:37 -0300

    add encrypt and decrypt from core

--- rsa_core.py
+++ rsa_core.py
@@ -1,11 +1,9 @@
 import number_theory
 
-def encrpyt(message, PE, mod):
+def encrypt(M, PE, mod):
     # (M^e mod n)
-    return number_theory.bin_pow(message, PE, mod)
+    return number_theory.bin_pow(M, PE, mod)
 
-def decrypt(ciphertext, PV, mod):
+def decrypt(C, PV, mod):
     # (C^d mod n)
-    return number_theory.bin_pow(ciphertext, PV, mod)
-
-
+    return number_theory.bin_pow(C, PV, mod)
